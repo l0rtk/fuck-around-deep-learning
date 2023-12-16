7a42dc4ffd9b710456e23ebd8bc68046953bc891
luka <edulukalortk@gmail.com>
2023-12-16 16:50:00 +0400

    __r(ops)__ fucntional behavios

--- gradinho/gradinho.py
+++ gradinho/gradinho.py
@@ -4,7 +4,7 @@ import math
 class Scalar:
     def __init__(self, value,label = '', _children = (), _op = ''):
         self.value = value
-        self._prev = _children 
+        self._prev = set(_children) 
         self._op = _op
         self.grad = 0.0
         self.label = label
@@ -14,30 +14,18 @@ class Scalar:
         out = self.value + other.value
         return Scalar(out, _children = (self, other), _op = '+')
 
-    def __radd__(self, other):
-        other = other if isinstance(other, Scalar) else Scalar(other)
-        out = self.value + other.value
-        return Scalar(out, _children = (self, other), _op = '+')
-    
+
     def __sub__(self, other):
         other = other if isinstance(other, Scalar) else Scalar(other)
         out = self.value - other.value
         return Scalar(out, _children = (self, other), _op = '-')
     
-    def __rsub__(self, other):
-        other = other if isinstance(other, Scalar) else Scalar(other)
-        out = other.value - self.value
-        return Scalar(out, _children = (self, other), _op = '-')
-        
+   
     def __mul__(self, other):
         other = other if isinstance(other, Scalar) else Scalar(other)
         out = self.value * other.value
         return Scalar(out, _children = (self, other), _op = '*')
 
-    def __rmul__(self, other):
-        other = other if isinstance(other, Scalar) else Scalar(other)
-        out = self.value * other.value
-        return Scalar(out, _children = (self, other), _op = '*')
 
     def __pow__(self, other):
         other = other if isinstance(other, Scalar) else Scalar(other)
@@ -47,6 +35,15 @@ class Scalar:
     def tanh(self):
         return Scalar(math.tanh(self.value), _children = (self, ), _op = "tanh")
         
+    def __radd__(self, other):
+        return self.value + other
+    
+    def __rsub__(self, other):
+        return self.value - other
+
+    def __rmul__(self, other):
+        return self.value * other
+
     
     def __repr__(self):
         return f"Scalar(value={str(self.value)})"
